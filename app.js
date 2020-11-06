3393f9450031bb828652efcdd7a31b2bcb811f8b
Jón Kristjánsson <busli@Jons-MacBook-Pro.local>
2020-11-06 22:29:36 +0000

    feat: add CORS headers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.get('*', (req, res) => {
   res.append('Content-Type', 'application/rss+xml; charset=UTF-8');
+  res.append('Access-Control-Allow-Origin', '*');
   res.sendFile(`${__dirname}/static/normid.xml`);
 });
 
