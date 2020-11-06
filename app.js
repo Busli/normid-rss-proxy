94006f1dc6c0db734363ef6c72483c68b5501e51
Jón Kristjánsson <busli@Jons-MacBook-Pro.local>
2020-11-06 22:36:53 +0000

    fix: set headers correctly

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// Add CORS headers
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('*', (req, res) => {
   res.set('Content-Type', 'application/rss+xml; charset=UTF-8');
-  res.set('Access-Control-Allow-Origin', '*');
   res.sendFile(`${__dirname}/static/normid.xml`);
 });
 
