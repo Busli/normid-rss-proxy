002307f094c25ecf2a6f3fff6fb37f95976348df
Jón Þór Kristjánsson <kristjansson91@gmail.com>
2021-06-02 10:42:40 +0000

    fix: proxy file since Vercel can not access file system

--- src/pages/api/feed.ts
+++ src/pages/api/feed.ts
@@ -1,32 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import { getDay, getHours, differenceInDays } from 'date-fns';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const currentDate = new Date();
-  const filePath = path.resolve('.', 'public/normid.xml');
-  const fileExists = fs.existsSync(filePath);
-  let lastMod = new Date();
-
-  if (fileExists) {
-    const { mtime } = fs.statSync(filePath); // mtime is when data was last modified
-    lastMod = mtime;
-  }
-
-  const day = getDay(currentDate); // Föstudagur er 5
-  const hour = getHours(currentDate);
-  const diff = differenceInDays(currentDate, lastMod);
-
-  if ((day === 5 && hour >= 7 && diff > 0) || !fileExists) {
-    const feed = await axios.get(
-      'https://normidpodcast.com/category/normid/feed/',
-    );
-    fs.writeFileSync(filePath, feed.data);
-  }
-
-  const fileBuffer = fs.readFileSync(filePath);
+  const feed = await axios.get(
+    'https://normidpodcast.com/category/normid/feed/'
+  );
   res.setHeader('Content-Type', 'application/rss+xml; charset=UTF-8');
-  res.send(fileBuffer);
+  res.send(feed.data);
 };
